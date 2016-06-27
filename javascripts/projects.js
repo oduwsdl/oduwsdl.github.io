0ba107982f076471167203cf18588f55b80e58bc
Mat Kelly <machawk1@gmail.com>
2016-06-27 16:59:21 -0400

    Don't change case

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,5 +1,6 @@
 String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    //return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return this;
 };
 
 $(document).ready(function(){
